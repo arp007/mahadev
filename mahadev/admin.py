b733f276426e3f89e13b3338a3cda85c43d821a3
anish <anish@anish-Vostro-1520.(none)>
2014-08-21 00:23:21 -0700

    added to djano admin

--- mahadev/admin.py
+++ mahadev/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from mahadev.models import Episode, Series, SeriesSequence
 
-admin.site.register(Episode)
-admin.site.register(Series)
-admin.site.register(SeriesSequence)
+class EpisodeAdmin(admin.ModelAdmin):
+	list_display = ['name', 'date']
+
+admin.site.register(Episode, EpisodeAdmin)
+
+class SeriesAdmin(admin.ModelAdmin):
+	list_display = ['link', 'date']
+admin.site.register(Series, SeriesAdmin)
+
+class SeriesSequenceAdmin(admin.ModelAdmin):
+	list_display = ['seq_num', 'crt_dt']
+
+admin.site.register(SeriesSequence, SeriesSequenceAdmin)
